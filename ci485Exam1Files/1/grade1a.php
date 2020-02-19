386bbf12c22f022d2a514591f43c123284fd7df4
Nizzy21 <61195224+Nizzy21@users.noreply.github.com>
2020-02-19 11:28:30 -0500

    myproject
    test

---
+++ ci485Exam1Files/1/grade1a.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<?php
+$fin=$_GET["fin"];
+$mid=$_GET["mid"];
+$choice=$_GET["choice"];
+?>
+<html>
+<head><title>GRADE1 PROBLEM1</title>
+<link rel="stylesheet" type="text/css" href="grade.css" />
+</head>
+<body>
+<div id="main"><div id="main2">
+<?php
+	if () echo "<form>"
+	else if () echo "<form>"
+?>
+<table>
+<th colspan="2">GRADE1 PROBLEM</th>
+<tr><td><label>SEMESTER:</label></td>		<td><select><option>Fall</option><option>Spring</option><option>Summer</option></select></td></tr>
+<tr>
+	<td colspan="2" > 
+	<input type="submit" value="Next"  style="color:white;background-color:black;border:4px solid white;" />
+	<input type="button" value="Back" 	 onclick="location.href='grade1.html'" style="color:white;background-color:black;border:4px solid white;" />
+</td></tr>
+</table> 
+</form>
+<div><div>
+</body>
+</html>
---
+++ ci485Exam1Files/2/grade.css
@@ -0,0 +1,7 @@
+#main{width:100%;font-size: x-large;}
+#main2{width:380px;margin:0px auto;color:white; border:solid black 0px;}
+table{color:black;background-color:lightgray;border:6px solid black;border-width:4px;width:100%;}
+th{color:white;background-color:black;outline-style:solid;outline-width:2px;border:2px solid black;font-size:x-large;}
+select,input[type='text']{background-color:#DEDEE6;border:4px solid black;}
+button,input[type='button'],input[type='submit'],input[type='reset']{color:black;background-color:gray;border:4px solid black;}
+label{font-size:medium;margin:0px;}
